866748db6ed9031adc5d576b2cbc0247f6be193f
Michael Margolies <mmargolies978@gmail.com>
2023-01-08 10:25:52 -0500

    "Divided mortgage calculator into functions, small QoL fixes (clearing console, newlines)"

--- mortgage_calculator.js
+++ mortgage_calculator.js
@@ -4,66 +4,146 @@ function prompt(message) {
   console.log(`>> ${message}`);
 }
 
-function validateInput(userInput) {
-  return userInput.trimStart() === ''   ||
-         isNaN(Number(userInput))       ||
-         (Number(userInput) <= 0)       ||
-         Number(userInput) === Infinity ||
-         Number(userInput) === -Infinity;
+function greetUser() {
+  prompt('Welcome to Mortgage Calculator!\n');
 }
 
-prompt('Welcome to Mortgage Calculator!\n');
+function isInputValid(userInput) {
+  return userInput.trimStart() !== ''   &&
+         !isNaN(Number(userInput))      &&
+         !(Number(userInput) <= 0)      &&
+         Number(userInput) !== Infinity &&
+         Number(userInput) !== -Infinity;
+}
 
-while (true) {
+function formatNumberToHundreths(number) {
+  return number.toFixed(2);
+}
+
+function formatStringToHundreths(string) {
+  return Number(string).toFixed(2);
+}
+
+function getPrincipalAmount() {
   prompt('Please enter the principal loan amount:');
   prompt("(Don't comma separate your amount i.e. 10,000 -> 10000)");
-  let principalAmount = rlSync.question();
+  return rlSync.question();
+}
 
-  while (validateInput(principalAmount)) {
+function isPrincipalAmountValid(principal) {
+  while (!isInputValid(principal)) {
     prompt('Please provide a valid input:');
-    principalAmount = rlSync.question();
+    principal = rlSync.question();
   }
+  return principal;
+}
 
+function getDurationMonths() {
   prompt('Please enter the duration of your loan in months:');
-  let durationMonths = rlSync.question();
+  return rlSync.question();
+}
 
-  while (validateInput(durationMonths)) {
+function isDurationMonthsValid(months) {
+  while (!isInputValid(months)) {
     prompt('Please provide a valid input:');
-    durationMonths = rlSync.question();
+    months = rlSync.question();
   }
 
+  return months;
+}
+
+function durationMonthsToInteger(months) {
+  return parseInt(months, 10);
+}
+
+function getApr() {
   prompt('Lastly, enter the APR for your loan:');
   prompt ('(Example: 10 for 10%, 6.8 for 6.8%)');
-  let apr = rlSync.question();
+  return rlSync.question();
+}
 
-  while (validateInput(apr)) {
+function isAprValid(apr) {
+  while (!isInputValid(apr)) {
     prompt('Please provide a valid input:');
     apr = rlSync.question();
   }
+  return apr;
+}
 
-  let monthlyInterest = ((Number(apr)) / 12) / 100;
+function calcMonthlyInterest(apr) {
+  return ((Number(apr)) / 12) / 100;
+}
 
-  let monthlyPayment = Number(principalAmount) *
-                    (monthlyInterest / (1 - Math.pow(
-                    // eslint-disable-next-line indent
-                    (1 + monthlyInterest), (-Number(durationMonths)))));
+function calcMonthlyPayment(principal, monthlyInterest, duration) {
+  return Number(principal) *
+         (Number(monthlyInterest) / (1 - Math.pow(
+        (1 + Number(monthlyInterest)), (-Number(duration)))));
+}
 
-  let paymentTotal = monthlyPayment * Number(durationMonths);
-  let interestTotal = paymentTotal - Number(principalAmount);
+function calcPaymentTotal(monthlyPayment, durationMonths) {
+  return Number(monthlyPayment) * Number(durationMonths);
+}
 
-  monthlyPayment = monthlyPayment.toFixed(2);
-  paymentTotal = paymentTotal.toFixed(2);
-  interestTotal = interestTotal.toFixed(2);
-  principalAmount = Number(principalAmount).toFixed(2);
+function calcInterestTotal(paymentTotal, principal) {
+  return Number(paymentTotal) - Number(principal);
+}
 
-  prompt(`Your monthly payment is $${monthlyPayment} occuring over ` +
-        `${durationMonths} months.`);
+function printLoanCalcResult(
+  monthlyPayment, durationMonths, paymentTotal,
+  principalAmount, interestTotal
+) {
+  prompt(`Your monthly payment is $${monthlyPayment} occuring ` +
+  `over ${durationMonths} months.`);
+  prompt('------------------------------------');
+  prompt(`Your total payment is $${paymentTotal} ` +
+  `($${principalAmount} principal and $${interestTotal} interest).\n`);
+}
 
-  prompt(`Your total payment is $${paymentTotal} ($${principalAmount} ` +
-        `principal and $${interestTotal} interest).`);
-
+function askForAnotherCalc() {
   prompt('Would you like to calculate another loan? (y/n)');
-  let calcAgain = rlSync.question().toLowerCase();
+  return rlSync.question().toLowerCase();
+}
 
-  if (calcAgain[0] !== 'y' || calcAgain !== 'yes') break;
+function isCalcAgainValid(calcAgain) {
+  return calcAgain === 'y' || calcAgain === 'yes';
+}
+
+greetUser();
+
+while (true) {
+
+  let principalAmount = getPrincipalAmount();
+  principalAmount = isPrincipalAmountValid(principalAmount);
+
+  let durationMonths = getDurationMonths();
+  durationMonths = isDurationMonthsValid(durationMonths);
+  durationMonths = durationMonthsToInteger(durationMonths);
+
+  let apr = getApr();
+  apr = isAprValid(apr);
+
+  let monthlyInterest = calcMonthlyInterest(apr);
+
+  let monthlyPayment = calcMonthlyPayment(
+    principalAmount, monthlyInterest, durationMonths
+  );
+
+  let paymentTotal = calcPaymentTotal(monthlyPayment, durationMonths);
+
+  let interestTotal = calcInterestTotal(paymentTotal, principalAmount);
+
+  monthlyPayment = formatNumberToHundreths(monthlyPayment);
+  paymentTotal = formatNumberToHundreths(paymentTotal);
+  interestTotal = formatNumberToHundreths(interestTotal);
+  principalAmount = formatStringToHundreths(principalAmount);
+
+  console.log('\n');
+
+  printLoanCalcResult(
+    monthlyPayment, durationMonths, paymentTotal, principalAmount, interestTotal
+  );
+
+  let calcAgain = askForAnotherCalc();
+  if (!isCalcAgainValid(calcAgain)) break;
+  console.clear();
 }
